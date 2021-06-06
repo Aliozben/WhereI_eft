c2d7b94375c1101c8c44936c2e884297c1d12bc8
Aliozben <aliozben4@gmail.com>
2021-06-06 23:01:49 +0300

    post api is added

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,31 +12,8 @@ app.use(
     origin: true,
   })
 );
-app.post("/test", async (req: express.Request, res: express.Response) => {
-  console.log("test");
-  // console.log(req);
-  return req.body;
-});
 app.use(request);
-// (async () => {
-//   console.log("started");
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   console.log("started1");
-//   await page.goto("https://www.youtube.com/watch?v=sFFTbtqx5Mg", {
-//     waitUntil: "networkidle2",
-//   });
-//   console.log("started2");
-//   await page.screenshot({path: "e.png"});
-//   console.log("started3");
-//   const recorder = new puppeteerRecord();
-//   await recorder.init(page, "./video");
-//   await recorder.start();
-//   setTimeout(() => {
-//     recorder.stop();
-//     browser.close();
-//   }, 5000);
-// })();
+
 app.listen(5000, () => {
   console.log("port 5000 is running.");
 });
